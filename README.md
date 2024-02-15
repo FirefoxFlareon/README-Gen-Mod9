75b032615408b4035cfd38b57aaf3dc59ee5a719
Firefox <tshankgg@gmail.com>
2024-02-15 14:56:57 -0600

    added commas, finished functions, successfully generated README

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# README-Gen-Mod9
-Node.js Challenge: Professional README Generator
+# README-Generator
+  
+  ## Table of Contents
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Description
+
+  This project helps create professional README documents via command prompts
+
+
+  ## Installation
+  
+  This project requires you to run the command "npm install" after cloning
+
+
+  ## Usage
+
+  After insallation, simply run command "node index.js" in the terminal and answer the prompted questions
+
+
+  ## Contributions
+  
+  n/a
+
+
+  ## Tests
+  
+  This project has no tests
+  
+
+  ## License
+  
+  MIT
+  
+
+  ## Questions
+  
+  Have questions about this project? Contact me!
+  Email me at: tshankgg@gmail.com,
+  or find me on GitHub here: https://github.com/FirefoxFlareon
--- index.js
+++ index.js
@@ -1,63 +1,66 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        name: "title"
+        name: "title",
         message: "What is your project's title?"
-    }
+    },
     {
         type: "input",
-        name: "description"
+        name: "description",
         message: "Write a description of your project"
-    }
+    },
     {
         type: "input",
-        name: "installation"
+        name: "installation",
         message: "Are there any required steps for installation?"
-    }
+    },
     {
         type: "input",
-        name: "usage"
+        name: "usage",
         message: "How to use this project"
-    }
+    },
     {
         type: "input",
-        name: "contributions"
+        name: "contributions",
         message: "Credit any contributors"
-    }
+    },
     {
         type: "input",
-        name: "tests"
+        name: "tests",
         message: "Add any testing instructions here"
-    }
+    },
     {
         type: "input",
-        name: "license"
+        name: "license",
         message: "Which license are you using?"
-    }
+    },
     {
         type: "input",
-        name: "email"
+        name: "email",
         message: "What is your email address?"
-    }
+    },
     {
         type: "input",
-        name: "username"
+        name: "username",
         message: "What is your GitHub username?"
-    }
+    }];
 
-];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Your README has been generated!"))
+}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeData = generateMarkdown(answers);
+        writeToFile('README.md', readmeData);
+    });
+}
+
 init();
