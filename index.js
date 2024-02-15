969baa3558f099fcb9c3074f58c0f43fb4873e68
Firefox <tshankgg@gmail.com>
2024-02-15 13:58:35 -0600

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title"
+        message: "What is your project's title?"
+    }
+    {
+        type: "input",
+        name: "description"
+        message: "Write a description of your project"
+    }
+    {
+        type: "input",
+        name: "installation"
+        message: "Are there any required steps for installation?"
+    }
+    {
+        type: "input",
+        name: "usage"
+        message: "How to use this project"
+    }
+    {
+        type: "input",
+        name: "contributions"
+        message: "Credit any contributors"
+    }
+    {
+        type: "input",
+        name: "tests"
+        message: "Add any testing instructions here"
+    }
+    {
+        type: "input",
+        name: "license"
+        message: "Which license are you using?"
+    }
+    {
+        type: "input",
+        name: "email"
+        message: "What is your email address?"
+    }
+    {
+        type: "input",
+        name: "username"
+        message: "What is your GitHub username?"
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
